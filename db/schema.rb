e8833aab7553dc62652ed249406b8fcbc246adf2
laura hallam <laura_hallam@yahoo.co.uk>
2018-02-18 18:41:27 +0000

    adding the testimonials table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218145936) do
+ActiveRecord::Schema.define(version: 20180218183917) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.string "name"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
