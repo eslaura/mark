9834a351a34286ebe69f4d5010ba96afa57a14bf
laura hallam <laura_hallam@yahoo.co.uk>
2018-02-18 15:12:18 +0000

    adding blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,24 @@
 class BlogsController < ApplicationController
+  def index         # GET /restaurants
+    @blogs = Blog.all
+  end
+
+  def show          # GET /blogs/:id
+    @blog = Blog.find(params[:id])
+  end
+
+  def new           # GET /restaurants/new
+  end
+
+  def create        # POST /restaurants
+  end
+
+  def edit          # GET /restaurants/:id/edit
+  end
+
+  def update        # PATCH /restaurants/:id
+  end
+
+  def destroy       # DELETE /restaurants/:id
+  end
 end
