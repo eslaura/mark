def757d0e4aeba87856fd950db2317b7f4202b80
laura hallam <laura_hallam@yahoo.co.uk>
2018-03-11 15:55:31 +0000

    adding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :blogs
+    resources :testimonials
+
+    root to: "blogs#index"
+  end
+
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
 
